f3aef066371937258a25d11ee22f1f10b5fb9155
Jenish Kothari <kotharijenish2001@gmail.com>
2024-08-23 12:45:10 -0400

    Skill sections complete - Portfolio

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import Navbar from "./components/Navbar/Navbar";
 import "./App.css";
 import Hero from "./components/Hero/Hero";
+import Skills from "./components/Skills/Skills";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero/>
+      <Skills/>
     </div>
   );
 }
