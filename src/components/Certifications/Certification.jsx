e3b58f362e0579aff6e219ec8455d8d22ed88d95
Jenish Kothari <kotharijenish2001@gmail.com>
2024-08-23 14:42:16 -0400

    Certification - Section - Portfolio.

---
+++ src/components/Certifications/Certification.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Certification.css";
+import saa from "../../assets/saa.svg";
+
+const Certification = () => {
+  return (
+    <div className='certification-section'>
+        <div className='certification-title'>
+            <h1>My Certifications</h1>
+        </div>
+        <div className='certification-data'>
+            <img src = "" alt=''></img>
+            <img src = "" alt=''></img>
+            <img src = "" alt=''></img>
+        </div>
+    </div>
+  )
+}
+
+export default Certification
